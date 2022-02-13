cb222a8608a699925392f68bccbf5bf59f68b709
Joshua Daza <jdaza@hugeinc.com>
2022-02-13 18:59:18 -0500

    Add templates for and style

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const expresshbs = require("express-handlebars");
 
 const app = express();
+
+// set habdlebars
+app.set('views', path.join(__dirname, 'views'))
+app.engine('hbs', expresshbs.create({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}).engine)
+app.set('view engine', 'hbs')
+// 
 
 app.use(morgan('dev'));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,37 +2,37 @@ const {db} = require('../firebase');
 const {Router} = require('express');
 const router = Router();
 
-router.get('/contact', async (req, res) => {
+router.get('/', async (req, res) => {
     const querySnapshot = await db.collection('contacts').get()
     const contactArr = querySnapshot.docs.map( doc => ({
         id: doc.id,
         firstname: doc.data().firstname,
         ...doc.data()
     }));
-    res.send(contactArr)
+    res.render('index', {contactArr});
 })
 
 router.post('/new-contact', async (req, res) => {
     const {firstname, lastname, email, phone} = req.body
     console.log(firstname, lastname, email, phone);
     await db.collection('contacts').add ({firstname, lastname, email, phone})
-    res.send('contact created')
+    res.redirect('/')
 })
 
 router.get('/edit-contact/:id', async (req, res) => {
-    await db.collection('contacts').doc(req.params.id).get()
-    res.send('contact founded')
+    const doc = await db.collection('contacts').doc(req.params.id).get()
+    res.render('index', {contact : {id: doc.id, ...doc.data()}})
+    
 })
 
 router.get('/delete-contact/:id', async (req, res) => {
-    await db.collection('contacts').doc(req.params.id).get()
-    res.send('contact deleted')
+    await db.collection('contacts').doc(req.params.id).delete()
+    res.redirect('/')
 })
 
 router.post('/update-contact/:id', async (req, res) => {
-    
     await db.collection('contacts').doc(req.params.id).update(req.body)
-    res.send('contact updated')
+    res.redirect('/')
 })
 
 module.exports =router;
